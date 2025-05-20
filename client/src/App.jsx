078c9136d884e0f0c7a5d0a5b327c62f5aa57a87
sahsujit <sujit8877123sah@gmail.com>
2025-05-20 19:06:10 +0545

    admin dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,13 @@ const Login = lazy(() => import("./pages/Login"));
 const Chat = lazy(() => import("./pages/Chat"));
 const Groups = lazy(() => import("./pages/Groups"));
 const NotFound = lazy(() => import("./pages/NotFound"));
+const AdminLogin = lazy(() => import("./pages/admin/AdminLogin"));
+const Dashboard = lazy(() => import("./pages/admin/Dashboard"));
+const UserManagement = lazy(() => import("./pages/admin/UserManagement"));
+const ChatManagement = lazy(() => import("./pages/admin/ChatManagement"));
+const MessagesManagement = lazy(() =>
+  import("./pages/admin/MessageManagement")
+);
 
 let user = true;
 const App = () => {
@@ -23,6 +30,12 @@ const App = () => {
         <Route path="/login" element={<ProtectRoute user={!user} redirect="/" >
           <Login />
         </ProtectRoute>} />
+
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route path="/admin/users" element={<UserManagement />} />
+          <Route path="/admin/chats" element={<ChatManagement />} />
+          <Route path="/admin/messages" element={<MessagesManagement />} />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
