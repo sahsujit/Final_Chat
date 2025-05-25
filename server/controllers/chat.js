aaa26b7782890498921e402f7039cd7bdf9302bc
sahsujit <sujit8877123sah@gmail.com>
2025-05-25 17:31:32 +0545

    chat controllers

--- server/controllers/chat.js
+++ server/controllers/chat.js
@@ -1,30 +1,175 @@
 import { ALERT, REFETCH_CHATS } from "../constants/event.js";
+import { getOtherMembers } from "../lib/helper.js";
 import { TryCatch } from "../middlewares/error.js";
-import {Chat} from "../models/chat.js";
+import { Chat } from "../models/chat.js";
+import { User } from "../models/user.js";
+
 import { emitEvent } from "../utils/features.js";
+import { ErrorHandler } from "../utils/utility.js";
 
-const newGroupChat = TryCatch(async(req, res, next)=>{
-    const {members, name} = req.body;
+const newGroupChat = TryCatch(async (req, res, next) => {
+  const { members, name } = req.body;
 
-    const allMembers = [...members, req.user];
+  const allMembers = [...members, req.user];
 
-    await Chat.create({
-        name,
-        groupChat: true,
-        creator: req.user,
-        members: allMembers
-    })
+  await Chat.create({
+    name,
+    groupChat: true,
+    creator: req.user,
+    members: allMembers,
+  });
+
+  emitEvent(req, ALERT, allMembers, `Welcome to ${name} group`);
+  emitEvent(req, REFETCH_CHATS, members);
+
+  return res.status(201).json({
+    success: true,
+    message: "Group Created",
+  });
+});
+
+const getMyChats = TryCatch(async (req, res, next) => {
+  const chats = await Chat.find({ members: req.user }).populate(
+    "members",
+    "name avatar"
+  );
+
+  const transformedChats = chats.map(({ _id, name, members, groupChat }) => {
+    const otherMember = getOtherMembers(members, req.user);
+    return {
+      _id,
+      groupChat,
+      avatar: groupChat
+        ? members.slice(0, 3).map(({ avatar }) => avatar.url)
+        : [otherMember.avatar.url],
+      name: groupChat ? name : otherMember.name,
+      members: members.reduce((prev, curr) => {
+        if (curr._id.toString() !== req.user.toString()) {
+          prev.push(curr._id);
+        }
+        return prev;
+      }, []),
+    };
+  });
+
+  return res.status(200).json({
+    success: true,
+    message: "Chats Fetched Successfully",
+    chats: transformedChats,
+  });
+});
+
+const getMyGroups = TryCatch(async (req, res, next) => {
+  const chats = await Chat.find({
+    members: req.user,
+    groupChat: true,
+    creator: req.user,
+  }).populate("members", "name avatar");
+
+  const groups = chats.map(({ _id, name, members, groupChat }) => {
+    return {
+      _id,
+      groupChat,
+      avatar: members.slice(0, 3).map(({ avatar }) => avatar.url),
+      name
+    }
+     
+  });
+
+
+  return res.status(200).json({
+    success: true,
+    message: "Groups Fetched Successfully",
+    groups,
+  });
+
+});
+
+
+
+const addMembers = TryCatch(async(req, res, next)=>{
+    const { chatId, members } = req.body;
+    const chat  = await Chat.findById(chatId);
     
-    emitEvent(req, ALERT,allMembers,`Welcome to ${name} group`);
-    emitEvent(req, REFETCH_CHATS, members)
+  if (!chat) return next(new ErrorHandler("Chat not found", 404));
+
+  if (!chat.groupChat)
+    return next(new ErrorHandler("This is not a group chat", 400));
+
+  if (chat.creator.toString() !== req.user.toString())
+    return next(new ErrorHandler("You are not allowed to add members", 403));
 
 
+  const allMembersPromise = members.map((i)=> User.findById(i, "name"))
 
-    return res.status(201).json({
-        success: true,
-        message: "Group Created",
-      });
+  const allNewMembers = await Promise.all(allMembersPromise);
+
+  const uniqueMembers = allNewMembers
+  .filter((i) => !chat.members.includes(i._id.toString()))
+  .map((i) => i._id);
+
+chat.members.push(...uniqueMembers);
+
+if (chat.members.length > 100)
+  return next(new ErrorHandler("Group members limit reached", 400));
+
+await chat.save()
+
+const allUsersName = allNewMembers.map((i)=>i.name).join(", ");
+
+emitEvent(
+    req,
+    ALERT,
+    chat.members,
+    `${allUsersName} has been added in the group`
+  );
+
+  emitEvent(req, REFETCH_CHATS, chat.members);
+
+  return res.status(200).json({
+    success: true,
+    message: "Members added successfully",
+  });
+
+});
+
+
+const removeMembers = TryCatch(async(req, res, next)=>{
+    const { chatId, userId } = req.body;
+
+    const [chat, userThatWillBeRemoved] = await Promise.all([
+        Chat.findById(chatId),
+        User.findById(userId,"name")
+    ])
+
+    
+  if (!chat) return next(new ErrorHandler("Chat not found", 404));
+
+  if (!chat.groupChat)
+    return next(new ErrorHandler("This is not a group chat", 400));
+
+  if (chat.creator.toString() !== req.user.toString())
+    return next(new ErrorHandler("You are not allowed to add members", 403));
+
+  if (chat.members.length <= 3)
+    return next(new ErrorHandler("Group must have at least 3 members", 400));
+
+  const allChatMembers = await chat.members.map((i) => i._id.toString());
+
+  chat.members = chat.members.filter((i)=> i._id.toString() !== userId.toString());
+  await chat.save();
+
+  emitEvent(req, ALERT, chat.members, {
+    message: `${userThatWillBeRemoved.name} has been removed from the group`,
+    chatId,
+  });
+
+  emitEvent(req, REFETCH_CHATS, allChatMembers);
+
+  return res.status(200).json({
+    success: true,
+    message: "Member removed successfully",
+  });
 })
 
-
-export {newGroupChat}
+export { newGroupChat, getMyChats, getMyGroups, addMembers, removeMembers };
--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { newGroupChat } from '../controllers/chat.js';
+import { newGroupChat,getMyChats, getMyGroups, addMembers, removeMembers} from '../controllers/chat.js';
 import { isAuthenticated } from '../middlewares/auth.js';
 const router = express.Router();
 
@@ -8,6 +8,11 @@ const router = express.Router();
 router.use(isAuthenticated)
 
 router.post('/new', newGroupChat)
+router.get('/my', getMyChats)
+router.get('/my/groups', getMyGroups)
+router.put('/addmembers', addMembers)
+router.put('/removemembers', removeMembers)
+
 
 
 export default router;
