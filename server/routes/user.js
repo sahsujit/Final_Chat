accbeefe64e5e18b1fe7e531e0e7f2f42cba09a2
sahsujit <sujit8877123sah@gmail.com>
2025-06-01 17:42:00 +0545

    user controller complete

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,20 +1,58 @@
-import express from 'express';
-import { newUser , login, getMyProfile, logout} from '../controllers/user.js';
-import { multerUpload } from '../middlewares/multer.js';
-import { isAuthenticated } from '../middlewares/auth.js';
-import { loginValidator, registerValidator, validateHandler } from '../lib/validators.js';
+import express from "express";
+import {
+  newUser,
+  login,
+  getMyProfile,
+  logout,
+  searchUser,
+  sendFriendRequest,
+  acceptFriendRequest,
+  getMyNotifications,
+  getMyFriends
+} from "../controllers/user.js";
+import { multerUpload } from "../middlewares/multer.js";
+import { isAuthenticated } from "../middlewares/auth.js";
+import {
+  acceptRequestValidator,
+  loginValidator,
+  registerValidator,
+  sendRequestValidator,
+  validateHandler,
+} from "../lib/validators.js";
 const router = express.Router();
 
+router.post(
+  "/new",
+  multerUpload,
+  registerValidator(),
+  validateHandler,
+  newUser
+);
+router.post("/login", loginValidator(), validateHandler, login);
+
+router.use(isAuthenticated);
+
+router.get("/me", getMyProfile);
+router.get("/logout", logout);
+
+router.get("/search", searchUser);
+
+router.put(
+  "/sendrequest",
+  sendRequestValidator(),
+  validateHandler,
+  sendFriendRequest
+);
+
+router.put(
+  " /acceptrequest",
+  acceptRequestValidator(),
+  validateHandler,
+  acceptFriendRequest
+);
 
 
-router.post('/new',multerUpload, registerValidator(),
- validateHandler, newUser)
-router.post('/login',loginValidator(), validateHandler, login)
-
-
-router.use(isAuthenticated)
-
-router.get('/me', getMyProfile)
-router.get('/logout', logout)
+router.get("/notifications", getMyNotifications);
+router.get("/friends", getMyFriends);
 
 export default router;
