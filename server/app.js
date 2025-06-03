66a98afd7ee60dc51b8a5461a787902db58a8c09
sahsujit <sujit8877123sah@gmail.com>
2025-06-03 17:46:31 +0545

    complete admin

--- server/app.js
+++ server/app.js
@@ -1,58 +1,88 @@
+import express from "express";
+import connectDB from "./utils/db.js";
+import dotenv from "dotenv";
+import { errorMiddleware } from "./middlewares/error.js";
+import cookieParser from "cookie-parser";
+import { createServer } from "http";
+import { v4 as uuid } from "uuid";
 
-import express from 'express';
-import connectDB from './utils/db.js';
-import dotenv from 'dotenv';
-import { errorMiddleware } from './middlewares/error.js';
-import cookieParser from 'cookie-parser';
 
+import { Server } from "socket.io";
 
-
-import userRoute from './routes/user.js';
-import chatRoute from './routes/chat.js';
-import adminRoute from './routes/admin.js';
-
+import userRoute from "./routes/user.js";
+import chatRoute from "./routes/chat.js";
+import adminRoute from "./routes/admin.js";
+import { NEW_MESSAGE } from "./constants/event.js";
 
 const app = express();
-
+const server = createServer(app);
+const io = new Server(server, {});
 
 dotenv.config({
-    path:"./.env",
+  path: "./.env",
 });
 
 const port = process.env.PORT || 5000;
 
+ const adminSecretKey = process.env.ADMIN_SECRET_KEY || "anonadmin";
+ const envMode = process.env.NODE_ENV.trim() || "PRODUCTION";
+const userSocketIDs = new Map();
 
 
-connectDB()
-
-
-
+connectDB();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
+app.use("/user", userRoute);
+app.use("/chat", chatRoute);
+app.use("/admin", adminRoute);
 
-app.use("/user", userRoute)
-app.use("/chat", chatRoute)
-app.use("/admin", adminRoute)
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
+app.use(errorMiddleware);
 
+io.on("connection", (socket) => {
 
+  const user = {
+    _id:"abcdefgh",
+    name:"Anon"
+  }
 
+  userSocketIDs.set(user._id, socket.id);
+  console.log("Connected to socket.io", socket.id);
 
+socket.on(NEW_MESSAGE, async({message, chatId, members})=>{
 
+  const messageForRealTime = {
+    content:message,
+     _id: uuid(),
+      sender: {
+        _id: user._id,
+        name: user.name,
+      },
+      chat: chatId,
+      createdAt: new Date().toISOString(),
+  }
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-}
+  const messageForDB = {
+      content: message,
+      sender: user._id,
+      chat: chatId,
+    };
+})
 
+  socket.on("disconnect", () => {
+    console.log("Disconnected from socket.io");
+  });
+});
 
-
-);
-
-
-app.use(errorMiddleware)
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-}
-);
+});
+
+
+
+export{userSocketIDs,adminSecretKey, envMode};
--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,7 +1,53 @@
+import { adminSecretKey } from "../app.js";
 import { TryCatch } from "../middlewares/error.js";
 import { Chat } from "../models/chat.js";
 import { Message } from "../models/message.js";
 import { User } from "../models/user.js";
+import { cookieOption } from "../utils/features.js";
+
+import jwt from "jsonwebtoken";
+
+
+
+const adminLogin = TryCatch(async(req, res, next)=>{
+   const { secretKey } = req.body;
+
+
+   const isMatched = secretKey === adminSecretKey;
+
+   if(!isMatched) return next(new ErrorHandler("Invalid Secret Key", 401));
+
+   const token = jwt.sign(secretKey, process.env.JWT_SECRET);
+
+    return res
+    .status(200)
+    .cookie("chattu-admin-token", token, cookieOption)
+    .json({
+      success: true,
+      message: "Authenticated Successfully, Welcome BOSS",
+    });
+
+
+})
+
+const adminLogout = TryCatch(async (req, res, next) => {
+  return res
+    .status(200)
+    .cookie("chattu-admin-token", "", {
+      ...cookieOption,
+      maxAge: 0,
+    })
+    .json({
+      success: true,
+      message: "Logged Out Successfully",
+    });
+});
+
+const getAdminData = TryCatch(async (req, res, next) => {
+  return res.status(200).json({
+    admin: true,
+  });
+});
 
 const allUsers = TryCatch(async (req, res, next) => {
   const users = await User.find({});
@@ -134,4 +180,4 @@ const getDashboardStats = TryCatch(async (req, res, next) => {
   });
 });
 
-export { allUsers, allChats, allMessages , getDashboardStats};
+export { allUsers, allChats, allMessages , getDashboardStats, adminLogin, adminLogout, getAdminData};
