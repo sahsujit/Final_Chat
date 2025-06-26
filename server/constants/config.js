3aa4bbb009461fd8d4df2fce290cbe30bf874932
Sujit Sah <129708170+sahsujit@users.noreply.github.com>
2025-06-26 14:59:16 +0545

    Update config.js

--- server/constants/config.js
+++ server/constants/config.js
@@ -2,13 +2,9 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
+    "https://final-chat-kohl.vercel.app", 
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
-
-
-const CHATTU_TOKEN = "chattu-token";
-
-export{corsOptions, CHATTU_TOKEN};
